3fab0c27dc2691cf48837ef6e23058e987dcf259
alvin <alvinviki9@gmail.com>
2023-04-29 18:39:02 +0700

    update tugas

---
+++ .vscode/Index.php
@@ -0,0 +1,16 @@
+<?php
+
+//tanda petik 2
+$nama_kamu = "budi";
+$umur = 5;
+echo "Hello Word. welcome".$nama_kamu."umur kamu". $umur. "tahun" ; 
+echo "<br/>";
+echo "berarti". $nama_kamu. "lahir di tahun";
+echo 2018- $umur;
+
+
+//Tanda petik 1
+echo 'hello word';
+
+
+?>
